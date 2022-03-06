203e2408942520e24205c09ab40e0fac31a1b059
Fatima Ezzahra <felemenoun@gmail.com>
2022-03-06 17:37:43 +0100

    Fix linter errors

--- src/component/getDataByDate/Date.jsx
+++ src/component/getDataByDate/Date.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import './date.css'
 
 const Date = () => {
+	const getDate = (e) => {
+		console.log(e.target.value);
+	}
 	return (
 		<div className="date_container d-flex-column">
 			<h1>Covid-19 Tracker</h1>
 			<div className="date_modal d-flex-column">
 				<span>Choose a Specific Day</span>
-				<input type="date" />
+				<input type="date" onChange={getDate}/>
 				<button type="button">Show stats</button>
 			</div>
 		</div>
