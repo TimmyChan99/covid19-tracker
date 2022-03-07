5e2c3e11e6f4a4ee2cde946dcba1ba409fcc4c7a
Fatima Ezzahra <felemenoun@gmail.com>
2022-03-07 16:00:25 +0100

    Refactor the API call code

--- src/api/covidTrackingAPI.js
+++ src/api/covidTrackingAPI.js
@@ -8,40 +8,65 @@ const getStatsFromAPI = async (selectedDate) => {
   const stringDate = selectedDate.toString();
   const { [stringDate]: date } = response.dates;
   const { countries } = date;
+  const { 
+    today_confirmed: totalTodayConfirmed,
+    today_new_deaths:  totaltodayNewDeaths,
+    today_new_recovered: totalTodayNewRecovered,
+    yesterday_deaths: totalYesterdayDeaths,
+  } = response.total;
   const totalStat = {
-    ...response.total, id: 'Total',
+    id: 'Total',
+    totalTodayConfirmed,
+    totaltodayNewDeaths,
+    totalTodayNewRecovered,
+    totalYesterdayDeaths,
   };
 
   const countriesStats = [];
-  console.log(Object.entries(countries));
-  for (const key in countries) {
-    const obj = {};
-    obj[key] = {};
-    const {
-      name,
-      today_confirmed: todayConfirmed,
-      today_new_deaths: todayNewDeaths,
-      today_new_recovered: todayNewRecovered,
-      yesterday_deaths: yesterdayDeaths,
-      today_vs_yesterday_open_cases: todayVsYesterdayOpenCases,
-      today_vs_yesterday_deaths: todayVsYesterdayDeaths,
-      today_vs_yesterday_confirmed: todayVsYesterdayConfirmed,
-      today_recovered: todayRecovered,
-    } = countries[key];
-
+  const arrayTest = Object.entries(countries);
+  arrayTest.forEach((item) => {
     countriesStats.push({
-      id: name,
-      name,
-      todayConfirmed,
-      todayNewDeaths,
-      todayNewRecovered,
-      yesterdayDeaths,
-      todayVsYesterdayOpenCases,
-      todayVsYesterdayDeaths,
-      todayVsYesterdayConfirmed,
-      todayRecovered,
-    });
-  }
+      id: item[0],
+      name: item[0],
+      todayConfirmed: item[1].today_confirmed,
+      todayNewDeaths: item[1].today_new_deaths,
+      todayNewRecovered: item[1].today_new_recovered,
+      yesterdayDeaths: item[1].yesterday_deaths,
+      todayVsYesterdayOpenCases: item[1].today_vs_yesterday_open_cases,
+      todayVsYesterdayDeaths: item[1].today_vs_yesterday_deaths,
+      todayVsYesterdayConfirmed: item[1].today_vs_yesterday_confirmed,
+      todayRecovered: item[1].today_recovered,
+    })
+  })
+  
+  // for (const key in countries) {
+    //   const obj = {};
+    //   obj[key] = {};
+    //   const {
+      //     name,
+  //     today_confirmed: todayConfirmed,
+  //     today_new_deaths: todayNewDeaths,
+  //     today_new_recovered: todayNewRecovered,
+  //     yesterday_deaths: yesterdayDeaths,
+  //     today_vs_yesterday_open_cases: todayVsYesterdayOpenCases,
+  //     today_vs_yesterday_deaths: todayVsYesterdayDeaths,
+  //     today_vs_yesterday_confirmed: todayVsYesterdayConfirmed,
+  //     today_recovered: todayRecovered,
+  //   } = countries[key];
+
+  //   countriesStats.push({
+  //     id: name,
+  //     name,
+  //     todayConfirmed,
+  //     todayNewDeaths,
+  //     todayNewRecovered,
+  //     yesterdayDeaths,
+  //     todayVsYesterdayOpenCases,
+  //     todayVsYesterdayDeaths,
+  //     todayVsYesterdayConfirmed,
+  //     todayRecovered,
+  //   });
+  // }
   countriesStats.push(totalStat);
   return countriesStats;
 };
--- src/component/detailsPage/Details.jsx
+++ src/component/detailsPage/Details.jsx
@@ -50,19 +50,19 @@ const Details = () => {
 				<span>today vs yesterday <br /> confirmed</span>
 			</div>
 			<div className="total_confirmed box-odd d-flex-column">
-				<span className="stat_number">{selectedCountry[1].today_confirmed.toExponential(2)}</span>
+				<span className="stat_number">{selectedCountry[1].totalTodayConfirmed.toExponential(2)}</span>
 				<span>Total confirmed</span>
 			</div>
 			<div className="total_new_death box d-flex-column">
-				<span className="stat_number">{selectedCountry[1].today_new_deaths}</span>
+				<span className="stat_number">{selectedCountry[1].totaltodayNewDeaths}</span>
 				<span>Total Death</span>
 			</div>
 			<div className="total_today_new_recovered box d-flex-column">
-				<span className="stat_number">{selectedCountry[1].today_new_recovered}</span>
+				<span className="stat_number">{selectedCountry[1].totalTodayNewRecovered}</span>
 				<span>total today <br /> new recovered</span>
 			</div>
 			<div className="total_yesterday_deaths box-odd d-flex-column">
-				<span className="stat_number">{selectedCountry[1].yesterday_deaths}</span>
+				<span className="stat_number">{selectedCountry[1].totalYesterdayDeaths}</span>
 				<span>total today <br /> new recovered</span>
 			</div>
 			<p>Data provided and used for the generation of these products
