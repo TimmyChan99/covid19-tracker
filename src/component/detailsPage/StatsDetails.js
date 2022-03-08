29bb230f86dc86d169a65ce55808302ed7c2914e
Fatima Ezzahra <felemenoun@gmail.com>
2022-03-08 18:10:00 +0100

    Style countries list

--- src/component/detailsPage/StatsDetails.js
+++ src/component/detailsPage/StatsDetails.js
@@ -1,30 +1,21 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import CountryBox from './CountryBox';
-import { FaVirus } from 'react-icons/fa';
+import { GiWorld } from 'react-icons/gi';
 
 const StatsDetails = () => {
-  
-  const countries = useSelector((state) => state.statsReducer);
-  const countryName = countries[0].name;
-  const selectedCountry = countries ? countries.filter((item) => (item.id === countryName || item.id === 'Total')) : defaultData;
 
   const ids = [1, 2,3,4,5,6,7,8];
   
   return (
     <div className="country_details">
-      <div className="headline">
-        <h2>COUNTRY</h2>
-        <div className="headline_stat d-flex-column">
-          <span>601</span>
-          <span>Today confirmed</span>
+      <div className="headline d-flex-row">
+        <GiWorld className="world"/>
+        <div className="headline_total">
+          <h1>Countries</h1>
+          <h3>Total  &nbsp; <span>66024</span></h3>
         </div>
-        <div className="headline_stat d-flex-column">
-          <span>601</span>
-          <span>Total confirmed</span>
-        </div>
-        <FaVirus />
-      </div>
+      </div>     
       <small>ALL STATS</small>
       <ul className="countries_list">
         { ids.map((item) => (
